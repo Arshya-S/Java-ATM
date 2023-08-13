f5094fbb14d43bcda822b142d29b01a6c5fdc466
Arshya Soleimani <arrshya@gmail.com>
2023-08-13 14:57:23 -0600

    Basic project files

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+// https://javahungry.blogspot.com/2019/12/java-projects-for-beginners.html
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
     }
 }
